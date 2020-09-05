b58ba92e9f40c39f607739f4ec796c712179dacf
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-05 10:58:27 -0500

    All tictactoe working properly

--- Lab04_TicTacToe/Classes/Position.cs
+++ Lab04_TicTacToe/Classes/Position.cs
@@ -6,18 +6,18 @@ namespace Lab04_TicTacToe.Classes
 {
     class Position
     {
-		public int Row { get; set; }
-		public int Column { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
 
-		/// <summary>
-		/// Position on the gameboard being initialized 
-		/// </summary>
-		/// <param name="row">row number</param>
-		/// <param name="column">column number</param>
-		public Position(int row, int column)
-		{
-			Row = row;
-			Column = column;
-		}
-	}
+        /// <summary>
+        /// Position on the gameboard being initialized 
+        /// </summary>
+        /// <param name="row">row number</param>
+        /// <param name="column">column number</param>
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
 }
